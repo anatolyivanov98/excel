088dbdafe3583fe853433e821282138b0ba33049
anatoly <tolyan011998@gmail.com>
2021-01-29 22:06:50 +0700

    Finish base framework functional

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,8 @@ const JSloaders = () => {
     {
       loader: 'babel-loader',
       options: {
-        presets: ['@babel/preset-env']
+        presets: ['@babel/preset-env'],
+        plugins: ['@babel/plugin-proposal-class-properties']
       }
     }
   ]
